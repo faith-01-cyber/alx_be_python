cb4de9e6553acab97c987cb954dce94e3de08f36
Faith Kemboi <kemboifaith941@gmail.com>
2025-11-06 19:27:28 +0300

    Add simple_interest.py for simple interest calculation

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+# Variables
+principal = 1000
+rate = 5
+time = 2
+
+# Calculate simple interest
+simple_interest = (principal * rate * time) / 100
+
+# Print the result
+print("The Simple Interest is", simple_interest)
